3f8f2975c69c28c7553818b5342a5a5e708188dd
nevikk <toliknev@mail.ru>
2023-02-12 06:12:28 +0300

    fix query to API

--- src/redux/asyncAction/hotels.js
+++ src/redux/asyncAction/hotels.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const fetchHotels = async ({location, checkIn, checkOut}) => {
-	const response = await axios.get(`http://engine.hotellook.com/api/v2/cache.json?location=${location}&currency=rub&checkIn=${checkIn}&checkOut=${checkOut}&token=7fcb3770011b991915caa97cb981887e`);
+	const response = await axios.get(`https://engine.hotellook.com/api/v2/cache.json?location=${location}&currency=rub&checkIn=${checkIn}&checkOut=${checkOut}&token=7fcb3770011b991915caa97cb981887e`);
 	return response.data;
 }
